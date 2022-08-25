ef505ed8d90524cc7548afe960afd2d367b3937b
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-08-25 18:25:15 +0300

    Fix Rubocop and Add i18n

--- Gemfile
+++ Gemfile
@@ -17,6 +17,8 @@ gem 'webpacker', '~> 5.0'
 
 gem 'turbolinks', '~> 5'
 
+gem 'i18n', '~> 1.12'
+
 gem 'jbuilder', '~> 2.7'
 
 gem 'bootsnap', '>= 1.4.4', require: false
--- app/models/book.rb
+++ app/models/book.rb
@@ -3,5 +3,5 @@ class Book < ApplicationRecord
 
   validate :title, presence: true, length: 2..80
   validate :description, presence: true, length: 2..500
-  validates :price, presence: true, :numericality => { :greater_than_or_equal_to => 0 }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
