90e529db370eb50b25498634b187fb75be75c44b
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-08-25 17:55:51 +0300

    Add simple validation

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,6 @@
 class Author < ApplicationRecord
   has_many :books, dependent: :destroy
+
+  validate :first_name, presence: true
+  validate :last_name, presence: true
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :author
+
+  validate :title, presence: true
+  validate :description, presence: true
+  validate :price, presence: true
 end
