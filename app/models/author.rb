aaeddf86ec45e02534e78eb7aae8c12b97fb0ca0
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-08-25 18:04:18 +0300

    Add validation langht

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,6 @@
 class Author < ApplicationRecord
   has_many :books, dependent: :destroy
 
-  validate :first_name, presence: true
-  validate :last_name, presence: true
+  validate :first_name, presence: true, length: 2..80
+  validate :last_name, presence: true, length: 2..80
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :author
 
-  validate :title, presence: true
-  validate :description, presence: true
-  validate :price, presence: true
+  validate :title, presence: true, length: 2..80
+  validate :description, presence: true, length: 2..500
+  validates :price, presence: true, :numericality => { :greater_than_or_equal_to => 0 }
 end
