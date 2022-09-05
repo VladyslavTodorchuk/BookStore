35236343ae792813ccd018033dfba3350cfabc42
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-09-05 15:03:29 +0300

    Fix all files

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,7 +5,6 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
-require 'capybara/rails'
 require 'webdrivers/chromedriver'
 require_relative 'support/configs/should_matcher'
 
