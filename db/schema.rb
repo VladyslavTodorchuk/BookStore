3a31c6ef76aa267c7254b30e54320ef900d35a07
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-09-05 14:22:13 +0300

    Add Should-matcher

--- db/schema.rb
+++ db/schema.rb
@@ -10,49 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_28_101722) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "authors", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "authors_books", force: :cascade do |t|
-    t.bigint "author_id"
-    t.bigint "book_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["author_id"], name: "index_authors_books_on_author_id"
-    t.index ["book_id"], name: "index_authors_books_on_book_id"
-  end
-
-  create_table "books", force: :cascade do |t|
-    t.string "title"
-    t.string "description"
-    t.float "price"
-    t.string "dimensions"
-    t.integer "year_of_publication"
-    t.string "materials"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "books_categories", force: :cascade do |t|
-    t.bigint "book_id"
-    t.bigint "category_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,106 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  shared_examples 'valid test' do
-    it 'valid' do
-      expect(my_book).to be_valid
-    end
-  end
-
-  shared_examples 'invalid test' do
-    it 'invalid' do
-      expect(my_book).not_to be_valid
-    end
-  end
-
-  shared_examples 'destroy' do
-    before do
-      my_book.destroy
-    end
-
-    it 'destroyed?' do
-      expect(my_book.destroyed?).to eq(result)
-    end
-  end
-
-  shared_examples 'save' do
-    it 'saved?' do
-      expect(my_book.save).to eq(result)
-    end
-  end
-
-  describe 'book valid' do
-    context 'when book valid' do
-      let(:my_book) do
-        FactoryBot.build(:book)
-      end
-
-      include_examples 'valid test'
-    end
-  end
-
-  describe 'book invalid' do
-    context 'when book invalid without title' do
-      let(:my_book) do
-        FactoryBot.build(:book, title: nil)
-      end
-
-      include_examples 'invalid test'
-    end
-
-    context 'when book invalid without description' do
-      let(:my_book) do
-        FactoryBot.build(:book, description: nil)
-      end
-
-      include_examples 'invalid test'
-    end
-
-    context 'when book invalid without price' do
-      let(:my_book) do
-        FactoryBot.build(:book, price: nil)
-      end
-
-      include_examples 'invalid test'
-    end
-
-    context 'when book invalid price is negative' do
-      let(:my_book) do
-        FactoryBot.build(:book, price: -1)
-      end
-
-      include_examples 'invalid test'
-    end
-  end
-
-  describe 'book save' do
-    context 'when book been saved' do
-      let(:my_book) do
-        FactoryBot.build(:book)
-      end
-      let(:result) { true }
-
-      include_examples 'save'
-    end
-
-    context 'when book not been saved' do
-      let(:my_book) do
-        FactoryBot.build(:book, price: nil)
-      end
-      let(:result) { false }
-
-      include_examples 'save'
-    end
-  end
-
-  describe 'book destroy' do
-    context 'when book been destroyed' do
-      let(:my_book) do
-        FactoryBot.build(:book)
-      end
-      let(:result) { true }
-
-      include_examples 'destroy'
+  describe 'books' do
+    context 'validations' do
+      it { should validate_presence_of(:title) }
+      it { should validate_presence_of(:description) }
+      it { should validate_presence_of(:price) }
+      it { should validate_presence_of(:year_of_publication) }
+      it { should validate_presence_of(:quantity) }
+      it { should validate_presence_of(:materials) }
+      it { should validate_presence_of(:dimensions) }
     end
   end
 end
