7ca7223937d8f41a1c342db52a8c4faeff3dfba1
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-09-05 18:49:39 +0300

    Add JS and Book Page

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   devise_for :users, as: 'omniauth', controller: { omniauth_callbacks: 'callbacks' }
 
   root 'home#index'
-
+  
   resources :users
 
   resources :books, only: %i[show index] do
