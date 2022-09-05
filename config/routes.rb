04438bea8b70a6849fcef66f88cfff34a263efbb
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-09-05 19:21:50 +0300

    Featire specs

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   devise_for :users, as: 'omniauth', controller: { omniauth_callbacks: 'callbacks' }
 
   root 'home#index'
-
+  
   resources :orders, only: %i[index] do
     get '/add_to_order', to: 'order_books#add_to_order'
     delete '/delete_from_cart/:book_id', to: 'order_books#delete_product'
@@ -22,7 +22,7 @@ Rails.application.routes.draw do
   end
 
   get '/coupon_check', to: 'coupons#find'
-
+  
   resources :users
 
   resources :books, only: %i[show index] do
