02ba20c82032401aec2a92a73566943bb18e8c38
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-09-05 19:21:50 +0300

    Featire specs

--- config/routes.rb
+++ config/routes.rb
@@ -14,24 +14,15 @@ Rails.application.routes.draw do
   devise_for :users, as: 'omniauth', controller: { omniauth_callbacks: 'callbacks' }
 
   root 'home#index'
-<<<<<<< HEAD
 
   resources :orders, only: %i[index show update] do
-=======
-  
-  resources :orders, only: %i[index] do
->>>>>>> a1af5ebb (Add Modle Order, OrderBook, Coupon)
     get '/add_to_order', to: 'order_books#add_to_order'
     delete '/delete_from_cart/:book_id', to: 'order_books#delete_product'
     put '/order_update_book', to: 'order_books#update_quantity'
   end
 
   get '/coupon_check', to: 'coupons#find'
-<<<<<<< HEAD
-  
-=======
 
->>>>>>> a1af5ebb (Add Modle Order, OrderBook, Coupon)
   resources :users
 
   resources :books, only: %i[show index] do
