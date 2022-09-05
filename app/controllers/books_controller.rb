9d9087a9f5e60b77d85726f1ad9012c3c0a56f9c
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-09-05 18:24:57 +0300

    Add controller and sorting quuery

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,25 @@
+class BooksController < ApplicationController
+  def index
+    @books_count = Book.count
+    @categories = Category.all
+
+    @books = BookQuery.query(params).paginate(page: params[:page], per_page: ApplicationHelper::PAGINATION_PER_PAGE)
+
+    respond_to do |format|
+      format.html
+      format.json do
+        render json: {
+          entries: render_to_string(partial: 'books', formats: [:html]),
+          pagination: view_context.will_paginate(@books)
+        }
+      end
+    end
+  end
+
+  def show
+    @book = Book.find_by(id: params[:id])
+
+    redirect_to books_path if @book.nil?
+  end
+end
+
