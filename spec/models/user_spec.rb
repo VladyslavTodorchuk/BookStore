b148a216a688891a1547325723a5115b364606e1
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-09-06 14:20:18 +0300

    Add User modle

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,12 @@
 RSpec.describe User, type: :model do
+<<<<<<< HEAD
   describe 'author' do
     context 'when validations' do
       it { is_expected.to validate_presence_of(:email) }
       it { is_expected.to validate_presence_of(:password) }
     end
   end
+=======
+  pending "add some examples to (or delete) #{__FILE__}"
+>>>>>>> 6b9ca2b (Add User modle)
 end
