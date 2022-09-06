b83cb672b29221de1725e5d42f224961e19c7c7a
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-09-06 14:20:18 +0300

    Add User modle

--- db/migrate/20220906110249_devise_create_users.rb
+++ db/migrate/20220906110249_devise_create_users.rb
@@ -3,6 +3,7 @@
 class DeviseCreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
+
       t.string :first_name, null: false
       t.string :last_name, null: false
       ## Database authenticatable
--- db/schema.rb
+++ db/schema.rb
@@ -139,6 +139,20 @@ ActiveRecord::Schema.define(version: 2022_09_13_083320) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
   add_foreign_key "authors_books", "authors"
   add_foreign_key "authors_books", "books"
   add_foreign_key "billings", "users"
