bfd0061f5e88bf792654e5d4835bf815f8ab4c2c
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-09-06 10:05:10 +0300

    Add association to decorator authors and categories

--- app/decorators/book_decorator.rb
+++ app/decorators/book_decorator.rb
@@ -1,13 +1,18 @@
 class BookDecorator < Draper::Decorator
   decorates_association :authors
+  decorates_association :categories
+
+  def self.collection_decorator_class
+    PaginatingDecorator
+  end
 
   delegate_all
 
   def authors_names
-    authors.map(&:full_name).join(', ')
+    authors.map(&:full_name)
   end
 
   def categories_names
-    categories.map(&:name).join(', ')
+    categories.map(&:name)
   end
 end
