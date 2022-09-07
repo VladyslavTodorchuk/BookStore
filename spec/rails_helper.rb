f8e9801d0f1583e5531c949e8ba2992571c2b97c
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-09-07 16:31:51 +0300

    Add User model spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -25,7 +25,6 @@ RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :view
   config.include Warden::Test::Helpers
   config.include Capybara::DSL
-
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
