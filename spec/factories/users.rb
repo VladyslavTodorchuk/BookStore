e874bce4e8493aa4a88c27c58ee53f3872ee6a74
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-09-07 19:12:01 +0300

    Add Rspe for header

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     email do
-      "#{FFaker::Name.first_name.downcase}.#{FFaker::Name.last_name.downcase}@#{FFaker::Company.name.downcase}.com"
+      "#{FFaker::Name.first_name.downcase}.#{FFaker::Name.last_name.downcase}@rspec.com"
     end
     password { 'password' }
   end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,7 @@ require_relative '../config/environment'
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 require 'shoulda-matchers'
+require 'devise'
 
 require_relative 'support/configs/shoulda_matcher'
 require_relative 'support/configs/capybara'
