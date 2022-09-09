4ab4ffecb833fb5cef64b897104d32c497695dc5
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-09-09 14:27:09 +0300

    Add modles for Shipping and Bolling

---
+++ db/migrate/20220909110556_create_billings.rb
@@ -0,0 +1,18 @@
+class CreateBillings < ActiveRecord::Migration[6.1]
+  def change
+    create_table :billings do |t|
+      t.references :user, foreign_key: true
+
+      t.string :first_name, null: false, default: ''
+      t.string :last_name, null: false, default: ''
+
+      t.string :address, null: false
+      t.string :city, null: false
+      t.integer :zip, null: false
+      t.string :country, null: false
+      t.string :phone, null: false
+
+      t.timestamps
+    end
+  end
+end
