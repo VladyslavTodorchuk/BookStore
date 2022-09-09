6ace876d125635df5669b8e8039a89bd266ae6e4
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-09-09 14:27:09 +0300

    Add modles for Shipping and Bolling

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,5 +4,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook,
            Rails.application.credentials.dig(:facebook, :FACEBOOK_ID),
            Rails.application.credentials.dig(:facebook, :FACEBOOK_SECRET),
-           scope: 'email', info_fields: 'email,name'
+           scope: 'public_profile, email', info_fields: 'email, first_name, last_name'
 end
