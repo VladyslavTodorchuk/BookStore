5f3c02450da7e1431d1af1c4a864e14ba14883d9
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-09-11 14:33:03 +0300

    Add Edit page with working understand button and remove function

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,5 +4,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook,
            Rails.application.credentials.dig(:facebook, :FACEBOOK_ID),
            Rails.application.credentials.dig(:facebook, :FACEBOOK_SECRET),
-           scope: 'public_profile, email', info_fields: 'email, first_name, last_name'
+           scope: 'email', info_fields: 'email,name'
 end
