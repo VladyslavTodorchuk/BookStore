b78acc2c7dc8c08be3ebec2643405c206294ff35
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-09-12 14:54:20 +0300

    Add user billing and shipping update

--- db/migrate/20220909110537_create_shippings.rb
+++ db/migrate/20220909110537_create_shippings.rb
@@ -3,8 +3,8 @@ class CreateShippings < ActiveRecord::Migration[6.1]
     create_table :shippings do |t|
       t.references :user, foreign_key: true
 
-      t.string :first_name, null: false, default: ''
-      t.string :last_name, null: false, default: ''
+      t.string :first_name
+      t.string :last_name
 
       t.string :address, null: false
       t.string :city, null: false
