faa11e399a784c1afb725e4c38aab7f1c490119c
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-09-12 16:57:06 +0300

    Add settings spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,6 @@
 RSpec.describe User, type: :model do
+  let(:user) { FactoryBot.create(:user) }
+
   describe 'author' do
     context 'when validations' do
       it { is_expected.to validate_presence_of(:email) }
