3d6f3b9493d6b30ed39a782d5e889a46b11a60dc
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-09-13 11:16:21 +0300

    Fix specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,6 @@ class User < ApplicationRecord
   has_one :billing, dependent: :destroy
   has_one :shipping, dependent: :destroy
 
-  accepts_nested_attributes_for :billing
-  accepts_nested_attributes_for :shipping
-
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.provider = auth.provider
