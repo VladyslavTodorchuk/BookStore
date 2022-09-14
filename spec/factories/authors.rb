bb8785f575a5be5ae55b7dbf425a494e15e944c9
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-09-14 11:32:17 +0300

    Add FactoryBot.create with associations with

--- spec/factories/authors.rb
+++ spec/factories/authors.rb
@@ -3,9 +3,14 @@ FactoryBot.define do
     first_name { FFaker::Name.first_name }
     last_name { FFaker::Name.last_name }
 
-    trait :with_books do
-      book
-      book_count { 1 }
+    factory :author_with_books do
+      transient do
+        books_count { 1 }
+      end
+
+      posts do
+        Array.new(books_count) { association(:books) }
+      end
     end
   end
 end
--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -7,5 +7,25 @@ FactoryBot.define do
     year_of_publication { FFaker::Number.rand(1900..2022) }
     dimensions { FFaker::Book.cover(:size) }
     quantity { FFaker::Number.rand(0..10) }
+
+    factory :book_with_authors do
+      transient do
+        authors_count { 1 }
+      end
+
+      posts do
+        Array.new(authors_count) { association(:author) }
+      end
+    end
+
+    factory :book_with_categories do
+      transient do
+        categories_count { 1 }
+      end
+
+      posts do
+        Array.new(categories_count) { association(:category) }
+      end
+    end
   end
 end
