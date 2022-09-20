4eed7805197011fe35a9b83ebcc4b7427f002f66
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-09-20 13:04:49 +0300

    Add Images rspec for Book images

--- spec/feature/admin/book_spec.rb
+++ spec/feature/admin/book_spec.rb
@@ -79,4 +79,26 @@ RSpec.describe 'Book' do
       expect(book.reload.images.count).to eq(0)
     end
   end
+
+  context 'when image' do
+    before do
+      click_link('Edit')
+
+      page.attach_file('book[images][]', './app/assets/images/Web-Book-Cover.png')
+
+      click_on 'Update Book'
+    end
+
+    it 'can add images to book' do
+      expect(book.reload.images.count).to eq(1)
+    end
+
+    it 'can delete images from book' do
+      click_link('Edit')
+
+      click_link('Delete')
+
+      expect(book.reload.images.count).to eq(0)
+    end
+  end
 end
