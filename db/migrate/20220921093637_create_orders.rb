cb477fdfeed688acc96e77a9b15f8b6f30f24e88
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-09-21 14:25:02 +0300

    Add Modle Order, OrderBook, Coupon

--- db/migrate/20220921093637_create_orders.rb
+++ db/migrate/20220921093637_create_orders.rb
@@ -3,9 +3,9 @@ class CreateOrders < ActiveRecord::Migration[6.1]
     create_table :orders do |t|
       t.references :user, null: true, foreign_key: true
       t.integer :total_price, default: 0
-
       t.string :status, default: 'initialized'
       t.datetime :last_action
+
       t.timestamps
     end
   end
