91319107cdb75fcfc8b13632a1a2b44bfb81caac
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-09-21 14:25:02 +0300

    Add Modle Order, OrderBook, Coupon

--- db/migrate/20220921093701_create_order_books.rb
+++ db/migrate/20220921093701_create_order_books.rb
@@ -4,6 +4,7 @@ class CreateOrderBooks < ActiveRecord::Migration[6.1]
       t.references :order, null: false, foreign_key: true
       t.references :book, null: false, foreign_key: true
       t.integer :quantity, default: 1
+
       t.timestamps
     end
   end
