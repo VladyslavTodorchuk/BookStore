ed4d185ceb18834761ef488bd9ae434a104ac21e
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-09-28 15:52:20 +0300

    Rebased

--- app/admin/books.rb
+++ app/admin/books.rb
@@ -32,7 +32,6 @@ ActiveAdmin.register Book do
       row :materials
       row :dimensions
       row :price, &:price
-      row :price_cents, &:price
       row :created_at
       row :updated_at
     end
