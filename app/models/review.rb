a1d6f001d548623b6a037d95c8405adaffc27797
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-10-02 17:03:48 +0300

    Fix rspec

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,11 +2,15 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :book
 
-  validates :title, presence: true, length: { maximum: 80 },
+  validates :title, presence: true, length: { maximum: Constants::VALIDATES_TITLE_LENGTH_MAX },
                     format: { with: %r{\A[A-Za-z0-9!#$%&'*+-/=?^_`{|}~. ]+\z}, message: I18n.t('review.errors.title') }
-  validates :body, presence: true, length: { maximum: 500 },
+  validates :body, presence: true, length: { maximum: Constants::VALIDATES_TEXT_LENGTH_MAX },
                    format: { with: %r{\A[A-Za-z0-9!#$%&'*+-/=?^_`{|}~. ]+\z}, message: I18n.t('review.errors.body') }
 
   validates :verified, inclusion: { in: [true, false] }, presence: true
-  validates :rating, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }, presence: true
+  validates :rating,
+            numericality: {
+              greater_than_or_equal_to: Constants::STAR_MIN,
+              less_than_or_equal_to: Constants::STAR_MAX
+            }, presence: true
 end
