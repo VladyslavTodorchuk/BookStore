0a5019a19ead21b61c7f37d70fa058eafdecf5a0
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-10-02 17:03:48 +0300

    Fix rspec

---
+++ spec/models/review_spec.rb
@@ -0,0 +1,17 @@
+RSpec.describe Review, type: :model do
+  describe 'review' do
+    context 'when validations' do
+      it { is_expected.to validate_presence_of(:title) }
+      it { is_expected.to validate_presence_of(:body) }
+      it { is_expected.to validate_presence_of(:rating) }
+      it { is_expected.to allow_values('a' * Constants::VALIDATES_TITLE_LENGTH_MAX).for(:title) }
+      it { is_expected.to allow_values('a' * Constants::VALIDATES_TEXT_LENGTH_MAX).for(:body) }
+      it { is_expected.to allow_values(1, 2, 3, 4, 5).for(:rating) }
+    end
+
+    context 'when associations' do
+      it { is_expected.to belong_to(:book) }
+      it { is_expected.to belong_to(:user) }
+    end
+  end
+end
