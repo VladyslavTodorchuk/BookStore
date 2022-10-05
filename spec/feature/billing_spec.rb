8b1ef1041e41cd44d698483b49ed2b1943ff8fa7
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-10-05 16:31:58 +0300

    Fix some required changes

--- spec/feature/billing_spec.rb
+++ spec/feature/billing_spec.rb
@@ -3,6 +3,7 @@ RSpec.describe 'Billing', type: :feature do
   include_context 'with api request global before and after hooks'
 
   let(:user) { create(:user) }
+  let(:attributes_for) { attributes_for(:billing) }
 
   describe '#fill_in' do
     it 'fill in billing' do
@@ -12,25 +13,25 @@ RSpec.describe 'Billing', type: :feature do
 
       within('div', id: 'billing') do
         find_all('div', class: 'form-group')[0] do
-          fill_in :first_name, with: FFaker::Name.first_name
+          fill_in :first_name, with: attributes_for[:first_name]
         end
         find_all('div', class: 'form-group')[1] do
-          fill_in :last_name, with: FFaker::Name.last_name
+          fill_in :last_name, with: attributes_for[:last_name]
         end
         find_all('div', class: 'form-group')[2] do
-          fill_in :address, with: FFaker::AddressUA.street_address
+          fill_in :address, with: attributes_for[:address]
         end
         find_all('div', class: 'form-group')[3] do
-          fill_in :city, with: FFaker::AddressUA.city
+          fill_in :city, with: attributes_for[:city]
         end
         find_all('div', class: 'form-group')[4] do
-          fill_in :zip, with: FFaker::AddressUA.zip_code
+          fill_in :zip, with: attributes_for[:zip]
         end
         find_all('div', class: 'form-group')[5] do
-          fill_in :country, with: FFaker::AddressUA.country
+          fill_in :country, with: attributes_for[:country]
         end
         find_all('div', class: 'form-group')[6] do
-          fill_in :phone, with: FFaker::PhoneNumberUA.phone_number
+          fill_in :phone, with: attributes_for[:phone]
         end
         click_on I18n.t('settings.save')
       end
