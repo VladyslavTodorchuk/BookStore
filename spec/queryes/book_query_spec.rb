2b2b2aeadf5bf903d340e92d699d18796b51890b
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-10-05 16:46:57 +0300

    Fix Megic string in query spec

--- spec/queryes/book_query_spec.rb
+++ spec/queryes/book_query_spec.rb
@@ -8,14 +8,19 @@ RSpec.describe BookQuery do
     end
   end
 
+  let(:titles) { %w[Adam Zimbabwe] }
+  let(:books_dates) do
+    ['Thu, 25 Aug 2022 15:25:38.761014000 UTC +00:00', 'Thu, 25 Aug 2022 14:20:38.761014000 UTC +00:00']
+  end
+
   describe 'all books sort' do
     context 'when sort by title A to Z' do
       let(:books) do
-        create(:book, title: 'Adam')
-        create(:book, title: 'Zimbabwe')
+        create(:book, title: titles.first)
+        create(:book, title: titles.second)
       end
 
-      let(:result) { 'Zimbabwe' }
+      let(:result) { titles.second }
       let(:test) { described_class.query({ sort: 'title DESC' }).first.title }
 
       include_examples 'sort test'
@@ -23,11 +28,11 @@ RSpec.describe BookQuery do
 
     context 'when sort title Z to A' do
       let(:books) do
-        create(:book, title: 'Adam')
-        create(:book, title: 'Zimbabwe')
+        create(:book, title: titles.first)
+        create(:book, title: titles.second)
       end
 
-      let(:result) { 'Adam' }
+      let(:result) { titles.first }
       let(:test) { described_class.query({ sort: 'title' }).first.title }
 
       include_examples 'sort test'
@@ -59,11 +64,11 @@ RSpec.describe BookQuery do
 
     context 'when sort by Newest' do
       let(:books) do
-        create(:book, created_at: 'Thu, 25 Aug 2022 14:20:38.761014000 UTC +00:00')
-        create(:book, created_at: 'Thu, 25 Aug 2022 15:25:38.761014000 UTC +00:00')
+        create(:book, created_at: books_dates.first)
+        create(:book, created_at: books_dates.second)
       end
 
-      let(:result) { 'Thu, 25 Aug 2022 15:25:38.761014000 UTC +00:00' }
+      let(:result) { books_dates.first }
       let(:test) { described_class.query({ sort: 'created_at DESC' }).first.created_at }
 
       include_examples 'sort test'
