6de2570f8d283cda249c8cf937ef3dc6b161e6d4
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-10-06 18:57:28 +0300

    Fix

--- db/schema.rb
+++ db/schema.rb
@@ -248,7 +248,6 @@ ActiveRecord::Schema.define(version: 2022_10_06_102140) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "provider"
     t.string "uid"
-
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
