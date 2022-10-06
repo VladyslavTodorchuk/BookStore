7bdb40878a9c1e2af0b0f92d52a3042b05dff2c0
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-10-06 15:28:36 +0300

    Fix all bugs

--- spec/feature/shipping_spec.rb
+++ spec/feature/shipping_spec.rb
@@ -13,25 +13,25 @@ RSpec.describe 'Shipping', type: :feature do
 
       within('div#shipping.col-md-5.mb-40') do
         find_all('div', class: 'form-group')[0] do
-          fill_in :first_name, with: attributes_for[:first_name]
+          fill_in :first_name, with: shipping_attributes[:first_name]
         end
-        find_all('div.form-group')[1] do
-          fill_in :last_name, with: attributes_for[:last_name]
+        find_all('div', class: 'form-group')[1] do
+          fill_in :last_name, with: shipping_attributes[:last_name]
         end
-        find_all('div.form-group')[2] do
-          fill_in :address, with: attributes_for[:address]
+        find_all('div', class: 'form-group')[2] do
+          fill_in :address, with: shipping_attributes[:address]
         end
-        find_all('div.form-group')[3] do
-          fill_in :city, with: attributes_for[:city]
+        find_all('div', class: 'form-group')[3] do
+          fill_in :city, with: shipping_attributes[:city]
         end
-        find_all('div.form-group')[4] do
-          fill_in :zip, with: attributes_for[:zip]
+        find_all('div', class: 'form-group')[4] do
+          fill_in :zip, with: shipping_attributes[:zip]
         end
-        find_all('div.form-group')[5] do
-          fill_in :country, with: attributes_for[:country]
+        find_all('div', class: 'form-group')[5] do
+          fill_in :country, with: shipping_attributes[:country]
         end
-        find_all('div.form-group')[6] do
-          fill_in :phone, with: attributes_for[:phone]
+        find_all('div', class: 'form-group')[6] do
+          fill_in :phone, with: shipping_attributes[:phone]
         end
         click_on I18n.t('settings.save')
       end
