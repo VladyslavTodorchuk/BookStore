40146d95202ec7720fbfe9ef272197e033c8f317
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-10-07 13:46:37 +0300

    Fix

--- app/admin/coupons.rb
+++ app/admin/coupons.rb
@@ -1,3 +1,36 @@
 ActiveAdmin.register Coupon do
   permit_params :code, :is_active, :active_till_date, :discount, :order_id
+
+  index do
+    selectable_column
+
+    column :code
+    column :is_active
+    column :active_till_date
+    column :discount
+
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :code
+      row :is_active
+      row :active_till_date
+      row :discount
+      row :order
+    end
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :code
+      f.input :is_active
+      f.input :active_till_date
+      f.input :discount
+      f.input :order
+    end
+
+    f.actions
+  end
 end
--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -1,3 +1,38 @@
 ActiveAdmin.register Order do
   permit_params :user_id, :coupon_id, :total_price, :status, :last_action
+
+  index do
+    selectable_column
+
+    column :user
+    column :coupon
+    column :total_price
+    column :status
+    column :last_action
+
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :user
+      row :coupon
+      row :total_price
+      row :status
+      row :last_action
+    end
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :user_id
+      f.input :coupon_id
+      f.input :total_price
+      f.input :status
+      f.input :last_action
+      f.input :books
+    end
+
+    f.actions
+  end
 end
