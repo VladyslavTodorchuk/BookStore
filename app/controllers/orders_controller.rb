25aebad46e4e649e7269049674b39cbcb88c4405
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-10-07 16:15:13 +0300

    Rebase

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,14 @@
 class OrdersController < ApplicationController
-  # skip_before_action :authenticate_user!, only: :edit
   def index
     @cart = Order.find(session[:order_id]).decorate
   end
 
   def show
-    @order = Order.find_by(id: session[:order_id], user_id: current_user.id).decorate
-    @user_billing = Billing.find_or_create_by(user_id: current_user.id)
-    @user_shipping = Shipping.find_or_create_by(user_id: current_user.id)
+    if current_user
+      @order = Order.find_by(id: session[:order_id], user_id: current_user.id).decorate
+      render 'orders/show'
+    else
+      redirect_to new_checkout_path
+    end
   end
 end
