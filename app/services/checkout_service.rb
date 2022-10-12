0b6e9754a0f50143fdfe9653eb109c1e875fe98e
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-10-12 13:22:57 +0300

    Checkout Add CreditCard Address Delivery method

--- app/services/checkout_service.rb
+++ app/services/checkout_service.rb
@@ -36,5 +36,30 @@ class CheckoutService
     else
       true
     end
+
+    def update_step(step, params)
+      case step
+      when :address then update_address(params[:permitted_params], params[:user])
+      when :payment then update_payment(params[:permitted_params], params[:user])
+      when :confirm then update_confirm(params[:permitted_params])
+      when :complete then update_complete(params[:permitted_params])
+      end
+    end
+
+    def update_address(permitted_params, current_user)
+      if permitted_params[:shipping].nil?
+        BillingUpdate.call(permitted_params: permitted_params, current_user: current_user)
+      else
+        ShippingUpdate.call(permitted_params: permitted_params, current_user: current_user)
+      end
+    end
+
+    def update_payment(permitted_params, current_user)
+      CreditCardUpdate.call(permitted_params: permitted_params, current_user: current_user)
+    end
+
+    def update_confirm(permitted_params); end
+
+    def update_complete(permitted_params); end
   end
 end
