812e9d41bdb904fff0b4b943189f0d26a9de85ad
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-10-17 14:15:53 +0300

    Add sort orders

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -11,6 +11,8 @@ class CheckoutsController < ApplicationController
 
     order = Order.find(session[:order_id])
 
+    order = Order.find(session[:order_id])
+
     if user.save
       sign_in(:user, user)
       user.send_reset_password_instructions
@@ -122,5 +124,59 @@ class CheckoutsController < ApplicationController
       shipping: %i[first_name last_name address city zip country phone],
       credit_card: %i[name code cvv expiration_date]
     )
+  end
+
+  def update_address(permitted_params)
+    if permitted_params[:shipping].nil?
+      update_billing(permitted_params[:billing])
+    else
+      update_shipping(permitted_params[:shipping])
+    end
+  end
+
+  def update_shipping(params)
+    if @user_shipping.update(params)
+      redirect_to checkout_path(step: :address), notice: 'Shipping was updated'
+    else
+      redirect_to checkout_path(step: :address), alert: CheckoutService.to_errors(@user_shipping.errors.messages)
+    end
+  end
+
+  def update_billing(params)
+    if @user_billing.update(params)
+      redirect_to checkout_path(step: :address), notice: 'Billing was updated'
+    else
+      redirect_to checkout_path(step: :address), alert: CheckoutService.to_errors(@user_billing.errors.messages)
+    end
+  end
+
+  def update_payment(permitted_params)
+    if @credit_card.update(permitted_params[:credit_card])
+      redirect_to checkout_path(step: :confirm)
+    else
+      redirect_to checkout_path(step: :payment), alert: CheckoutService.to_errors(@credit_card.errors.messages)
+    end
+  end
+
+  def update_confirm
+    @order.coupon&.update(is_active: false)
+
+    if @order.update(status: :created, total_price: count_total_price)
+      redirect_to root_path, notice: 'Order was created!'
+    else
+      redirect_to checkout_path(step: :confirm), alert: 'Something went wrong'
+    end
+  end
+
+  def count_total_price
+    product = OrderBook.where(order_id: @order.id)
+
+    total_price = product.map do |order|
+      order.book.price_cents * order.quantity
+    end.sum
+
+    total_price *= @order.coupon.discount unless @order.coupon.nil?
+
+    total_price + @order.delivery.price_cents
   end
 end
--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -5,6 +5,8 @@ class CouponsController < ApplicationController
     coupon = Coupon.find_by(code: params[:code])
     order = Order.find(session[:order_id])
 
+    coupon&.update(order_id: order.id)
+
     respond_to do |format|
       format.json do
         render json: CouponService.find_coupon(params, order, coupon)
