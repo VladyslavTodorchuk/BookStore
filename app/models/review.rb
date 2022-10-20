d623045d648ee458ec5093d1ca39769f9df84fe0
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-10-20 12:39:18 +0300

    Fix

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,10 +1,14 @@
 class Review < ApplicationRecord
+
+  VALIDATES_TEXT_LENGTH_MAX = 500
+  VALIDATES_TITLE_LENGTH_MAX = 80
+
   belongs_to :user
   belongs_to :book
 
-  validates :title, presence: true, length: { maximum: Constants::VALIDATES_TITLE_LENGTH_MAX },
+  validates :title, presence: true, length: { maximum: VALIDATES_TITLE_LENGTH_MAX },
                     format: { with: %r{\A[A-Za-z0-9!#$%&'*+-/=?^_`{|}~. ]+\z}, message: I18n.t('review.errors.title') }
-  validates :body, presence: true, length: { maximum: Constants::VALIDATES_TEXT_LENGTH_MAX },
+  validates :body, presence: true, length: { maximum: VALIDATES_TEXT_LENGTH_MAX },
                    format: { with: %r{\A[A-Za-z0-9!#$%&'*+-/=?^_`{|}~. ]+\z}, message: I18n.t('review.errors.body') }
 
   validates :verified, inclusion: { in: [true, false] }, presence: true
