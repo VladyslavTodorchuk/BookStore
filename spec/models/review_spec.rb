6b09a3700c3b7a88523c63bda2a4193346dd6091
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-10-20 12:39:18 +0300

    Fix

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe Review, type: :model do
       it { is_expected.to validate_presence_of(:body) }
       it { is_expected.to validate_presence_of(:rating) }
       it { is_expected.to validate_presence_of(:verified) }
-      it { is_expected.to allow_values('a' * Constants::VALIDATES_TITLE_LENGTH_MAX).for(:title) }
-      it { is_expected.to allow_values('a' * Constants::VALIDATES_TEXT_LENGTH_MAX).for(:body) }
+      it { is_expected.to allow_values('a' * Review::VALIDATES_TITLE_LENGTH_MAX).for(:title) }
+      it { is_expected.to allow_values('a' * Review::VALIDATES_TEXT_LENGTH_MAX).for(:body) }
       it { is_expected.to allow_values(1, 2, 3, 4, 5).for(:rating) }
     end
 
