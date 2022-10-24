039764af7507511da3774fe4028b773d55dc37fd
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-10-24 15:00:23 +0300

    Fix Mailer Error

--- spec/models/coupon_spec.rb
+++ spec/models/coupon_spec.rb
@@ -1,5 +1,5 @@
-require 'rails_helper'
-
 RSpec.describe Coupon, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'when associations' do
+    it { is_expected.to belong_to(:order).optional(true) }
+  end
 end
