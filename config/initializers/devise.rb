2d1b43f354206add8a85f6af8e4c4e35a612bd2d
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-11-07 13:19:49 +0200

    Fix database

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -2,11 +2,12 @@
 
 Devise.setup do |config|
   config.mailer_sender = ENV['EMAIL_SENDER']
+  config.action_mailer.delivery_method = :smtp
   require 'devise/orm/active_record'
   config.case_insensitive_keys = [:email]
   config.strip_whitespace_keys = [:email]
   config.skip_session_storage = [:http_auth]
-  config.stretches = Rails.env.test? ? 1 : 11
+  config.stretches = Rails.env.test? ? 1 : 12
   config.reconfirmable = true
   config.expire_all_remember_me_on_sign_out = true
   config.password_length = 6..128
