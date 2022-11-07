4af94e04ea0d623f116faaa498690e9f1c9ec8a3
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-11-07 13:21:31 +0200

    Fix database

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -2,7 +2,6 @@
 
 Devise.setup do |config|
   config.mailer_sender = ENV['EMAIL_SENDER']
-  config.action_mailer.delivery_method = :smtp
   require 'devise/orm/active_record'
   config.case_insensitive_keys = [:email]
   config.strip_whitespace_keys = [:email]
