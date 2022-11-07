5a1e3c4e85f0ab357f7a22186b656a194f22266d
Vladyslav <49914999+VladyslavTodorchuk@users.noreply.github.com>
2022-11-07 13:12:51 +0200

    Fix database

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,5 +25,5 @@ Rails.application.configure do
 
   config.active_record.dump_schema_after_migration = false
   config.active_job.queue_adapter = :sidekiq
-  config.action_mailer.default_url_options = { host: ENV['HOST_DOMAIN'], port: 3000 }
+  config.action_mailer.default_url_options = { host: ENV['HOST_DOMAIN'] }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 if Rails.env.development?
-  AdminUser.create!(email: 'admin@example.com', password: 'password',
+  AdminUser.create(email: 'admin@example.com', password: 'password',
                     password_confirmation: 'password')
 
   category = Category.create!(name: FFaker::Book.genre)
